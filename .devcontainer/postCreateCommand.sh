825f168d1b553d1eb9259078a78833f0b63d98b1
alfi0812 <admin@alfi0812.de>
2025-06-25 15:56:32 +0000

    lets see

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -12,10 +12,15 @@ fisher install nickeb96/puffer-fish
 fisher install PatrickF1/fzf.fish
 "
 
-curl -fsSLO "https://github.com/kubernetes-sigs/krew/releases/latest/download/krew-linux_amd64.tar.gz" \
-    && tar zxvf "krew-linux_amd64.tar.gz" \
-    && ./"krew-linux_amd64" install krew \
-    && rm ./krew-linux_amd64.tar.gz
+(
+  set -x; cd "$(mktemp -d)" &&
+  OS="$(uname | tr '[:upper:]' '[:lower:]')" &&
+  ARCH="$(uname -m | sed -e 's/x86_64/amd64/' -e 's/\(arm\)\(64\)\?.*/\1\2/' -e 's/aarch64$/arm64/')" &&
+  KREW="krew-${OS}_${ARCH}" &&
+  curl -fsSLO "https://github.com/kubernetes-sigs/krew/releases/latest/download/${KREW}.tar.gz" &&
+  tar zxvf "${KREW}.tar.gz" &&
+  ./"${KREW}" install krew
+)
 
 export PATH="${KREW_ROOT:-$HOME/.krew}/bin:$PATH"
 
